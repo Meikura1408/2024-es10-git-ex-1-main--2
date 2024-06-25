342c22b1c12fe051b95959acb2d011900fcc428c
Meikura1408 <patriziasolla0@gmail.com>
2024-06-25 15:07:15 +0200

    first commit

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        test();
+    }
+
+    public static void test() {
+        System.out.println("Hello, World!");
+    }
+}
